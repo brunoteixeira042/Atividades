aafcc5f604b72433b3284cf2bb3d770f57d447b1
brunoteixeira042 <brunoteixeira042@gmail.com>
2024-07-08 20:21:51 -0300

    atividades v.10

---
+++ exercícios JavaScript/Exercício01/calculadora.js
@@ -0,0 +1,10 @@
+let num1 = prompt("Digite o N1: ")
+let num2 = prompt("Digite o N2: ")
+soma = parseFloat(num1)+parseFloat(num2)
+subtracao = parseFloat(num1)-parseFloat(num2)
+multiplicacao = parseFloat(num1)*parseFloat(num2)
+divisao = parseFloat(num1)/parseFloat(num2)
+
+alert(
+"Adição: " + soma + " subtração: " + subtracao + " multiplicação: " + multiplicacao+ " divisão: " + divisao
+)
---
+++ exercícios JavaScript/Exercício01/recrutas.js
@@ -0,0 +1,12 @@
+
+let nome = prompt("Digite o nome: ")
+let sobreNome = prompt("Digite o Sobre Nome: ")
+let campoEstudo = prompt("Digite campo de estudo: ")
+let anoNascimento = prompt ("Digite Data de Nascimento: ")
+
+    alert(
+        nome +" " + sobreNome + " Estuda: " + campoEstudo + " e tem " + (2024-anoNascimento)+" anos"
+    )
+    
+
+
