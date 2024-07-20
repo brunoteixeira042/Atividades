95a5306dc338ad9c1ca9fa91870027a583ca9da6
brunoteixeira042 <brunoteixeira042@gmail.com>
2024-07-20 20:54:24 -0300

    v4.0

--- exercícios JavaScript/Exercício09/CalculadoraGeometrica.js
+++ exercícios JavaScript/Exercício09/CalculadoraGeometrica.js
@@ -1,51 +1,73 @@
-let imoveisCadastrados = []
-let NumeroImoveis = 0
-let opcao
-do {
-    opcao = prompt(
-    "Imoveis castrados: "+ imoveisCadastrados.length +    
-    "\n1.Salvar Imóvel" +
-    "\n2.Mostrar Imoveis Salvos" +
-    "\n3.Sair") 
-    switch (opcao) {
-        case "1":
-            const imovel =  {
-                nome: "nome",
-                quantidadeQuartos: 0,
-                quantidadeBanheiros: 0,
-                possuiGaragem:false,
-            }
-                imovel.nome = prompt("Digite o nome do imóvel: ")
-                imovel.quantidadeQuartos = prompt("Digite quantidade de Quartos: ")
-                imovel.quantidadeBanheiros = prompt("Digite quantidade de banheiros: ")
-                let possuiGaragem = prompt("Digite garagem (sim ou nao):  ")
-                if(possuiGaragem.toLowerCase() === "sim")
-                    imovel.possuiGaragem=true
-            
-            imoveisCadastrados.push(imovel)
-            
-            break;
-        case "2":
-            let nomeImoveis = ""
-            for(let i=0; i < imoveisCadastrados.length;i++){
-                let imovel = imoveisCadastrados[i]
+let escolha
+let calculo
 
-                nomeImoveis +=
-                    `nome: ${imovel.nome}`+
-                    `\nQuartos: ${imovel.quantidadeQuartos}`+
-                    `\nBanheiros: ${imovel.quantidadeBanheiros}`+ 
-                    `\nGaragem: ${imovel.possuiGaragem}`
-                    +"\n"   
-            }
-            alert(nomeImoveis)
+function calcularAreaTriangulo() {
+    let areaBase = prompt("Digite o valor da base: ")
+    let areaAltura = prompt("Digite o valor da altura: ")
+    return  (parseFloat(areaBase) * parseFloat(areaAltura) / 2)
+}
+function calcularAreaRetangulo() {
+    let areaBase = prompt("Digite o valor da base: ")
+    let areaAltura = prompt("Digite o valor da altura: ")
+    return parseFloat(areaBase) * parseFloat(areaAltura)
+    
+}
+
+function calcularAreaQuadrado() {
+    let areaLateral = prompt("Digite o valor do Lado: ")
+    return  (parseFloat(areaLateral) ** 2)
+    
+}
+
+function calcularAreaTrapezio() {
+    let areaBaseMenor = prompt("Digite o valor da base menor: ")
+    let areaBaseMaior = prompt("Digite o valor da base Maior: ")
+    let areaAltura = prompt("Digite o valor da Altura: ")
+    return((parseFloat(areaBaseMenor) + parseFloat(areaBaseMaior)) * parseFloat(areaAltura) / 2)
+}
+
+function calcularAreaCirculo() {
+    let areaRaio = prompt("Digite o valor do Raio: ")
+    return ((parseFloat(areaRaio) ** 2) * 3.14)
+}
+
+
+do {
+    escolha = prompt(
+        "Escolha a figura para calcular Área : " +
+        "\n1.Área do triângulo" +
+        "\n2.Área do Retângulo" +
+        "\n3.Área do Quadrado" +
+        "\n4.Área do Trapézio" +
+        "\n5.Área do Círculo" +
+        "\n6.Sair")
+    switch (escolha) {
+        case "1":
+           calculo = calcularAreaTriangulo()
+            alert("o valor da área do triângulo: " + calculo)
             break
+        case "2":
+            calculo = calcularAreaRetangulo()
+            alert("o valor da área do Retângulo: " + calculo)
+            break;
+
         case "3":
-            alert("Saindo")
+            calculo = calcularAreaQuadrado()
+            alert("o valor da área do Quadrado: " + calculo)
+            break
+        case "4":
+            calculo = calcularAreaTrapezio()
+            alert("o valor da área do Trapézio: " + calculo)
+            break
+        case "5":
+           calculo = calcularAreaCirculo()
+            alert("o valor da área do Círculo: " + calculo)
+            break;
+        case "6":
+            alert("Saindo..")
             break
         default:
             alert("Opção Invalida")
             break;
     }
-    
-
-} while (opcao!=="3");
+} while (escolha !== "6");
